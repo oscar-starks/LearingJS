0ca48a2f658ce2e464ab720d283ee247b428df8e
Blessed Oscar <oscarblessed04@gmail.com>
2023-08-09 12:54:30 +0100

    made the api calls more useable

--- NODE/reference/second_api_test.js
+++ NODE/reference/second_api_test.js
@@ -1,22 +1,20 @@
-const fetch = require('node-fetch'); // If you're running this in a Node.js environment
+const fetch = require('node-fetch'); 
 
 const url = 'http://localhost:8000/accounts/login/';
 const payload = {
-  email:"email@gmail.com",
-  password:"email",
+  email:"test@gmail.com",
+  password:"test",
 }
-
 
 fetch(url, {
   method: 'POST',
   body: new URLSearchParams(payload),
 })
+
   .then(response => {
-    if (!response.ok) {
-      console.log(response)
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
+    return response.json().then(data => {
+      return Object.assign({ status_code: response.status },(data))
+    });
   })
 
   .then(data => {
