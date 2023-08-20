cdd643f4face7f39be45b5b97a09e47327645f32
Blessed Oscar <oscarblessed04@gmail.com>
2023-08-20 18:14:46 +0100

    understanding writing a simple middleware

--- second_node_module/index.js
+++ second_node_module/index.js
@@ -1,8 +1,25 @@
 const path = require('path');
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 
+server.use(cors());
+
+server.use((req, res, next) => {
+    console.log(req.method)
+    console.log(req.headers.origin)
+    console.log(req.path)
+    console.log("I think this can act as a custom middleware")
+    next();
+});
+
+// this is used to handle url encoded data
+server.use(express.urlencoded({ extended:false}));
+// this is a middleware for json parsing
+server.use(express.json());
+// THIS IS for serving static files
+server.use(express.static(path.join(__dirname,'static/')));
 
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
@@ -17,10 +34,22 @@ server.get('/redirect/', (req, res) => {
     res.redirect(301,"/");
 });
 
+server.get('/chaining/', (req, res, next) => {
+    console.log("chaining attempted");
+    next();
+}, (err, res) => {
+    res.send("the chaining succeeded");
+});
+
 server.get('*/', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'src', '404.html'));
 });
 
+
+server.use(function(err, req, res, next) {
+    console.log(err.stack);
+    res.send(500).send(err.message);
+})
 
 server.listen(PORT,(err, server) => {
     console.log(`server listening on ${PORT}`);
